8d414c9668fad9d9c41e6710e3488fb260703c5e
Jessica Liang <jessicursliang@gmail.com>
2020-07-19 14:43:45 -0700

    deploy bug gunicorn

--- app.py
+++ app.py
@@ -21,18 +21,18 @@ app.config.from_mapping(
 )
 app.debug = True # so development server autoupdates with changes
 
-if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
+# if test_config is None:
+#     # load the instance config, if it exists, when not testing
+#     app.config.from_pyfile('config.py', silent=True)
+# else:
+#     # load the test config if passed in
+#     app.config.from_mapping(test_config)
 
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+# # ensure the instance folder exists
+# try:
+#     os.makedirs(app.instance_path)
+# except OSError:
+#     pass
 
 client = MongoClient('mongodb://127.0.0.1:27017')
 db = client.capstone
