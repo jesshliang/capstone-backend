721259ad5c6a9f2b01eeb9fb2ae5815bfedd50d0
Jessica Liang <jessicursliang@gmail.com>
2020-07-19 14:54:40 -0700

    deploy bug gunicorn

---
+++ build/precache-manifest.37263630a71bd4d57f86f1d8831ae495.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e474d174d9ff9ec0020d8933d094a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9844679c936dda5ee5a",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "beb37a38836b378fae00",
+    "url": "/static/css/main.63213394.chunk.css"
+  },
+  {
+    "revision": "d9844679c936dda5ee5a",
+    "url": "/static/js/2.143d8b86.chunk.js"
+  },
+  {
+    "revision": "a1212b395e88f070a42368ea2ff93e06",
+    "url": "/static/js/2.143d8b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb37a38836b378fae00",
+    "url": "/static/js/main.3e1b29d9.chunk.js"
+  },
+  {
+    "revision": "f33bc6beecfca2739a79",
+    "url": "/static/js/runtime-main.48c5c996.js"
+  }
+]);
